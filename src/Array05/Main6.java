113ac9168542bcd1d62f0f004f528d076842f08f
jakdalmak7961 <jakdalmak7961@gmail.com>
2024-08-13 05:00:02 +0900

    Array 6번 문제 2회차 시도 책 설명, 코드 참조하여 풀었음. 모르는 부분이 있어 스스로 푸는 것이 불가능... HashMap에 대한 stream()을 수행할 때 사용되는 코드들에 대해 공부하기(EntrySet(), HashMap.Entry::getKey) stream() 내에서 정렬을 위해 사용된 코드들에 대해 공부하기(Double.compare()) array를 보다 다양한 방식으로 활용할 수 있음을 이해하기(index를 특정 값을 나타내는 형식으로 사용. 0과 같은 일부 인덱스의 의도적 미사용 등)

--- src/Array05/Main6.java
+++ src/Array05/Main6.java
@@ -1,48 +1,40 @@
 package Array05;
 
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /** 풀이 실패에 따른 다음 챕터 진행 */
 public class Main6 {
 
     public static int[] solution(int N, int[] stages) {
-        int[] result = new int[N];
-        Map<Float, Integer> stageMap = new HashMap<>();
-        Float[] failPercentArr = new Float[N];
+        // stages.length == 본 게임의 전체 유저 수
 
+        Map<Integer, Double> stageFailureMap = new HashMap<>();
 
-        for(int i = 0; i < N; i++) {
-            int stage = i+1;
-            int herePlayer = (int) Arrays.stream(stages)
-                    .filter(m -> m == stage).count(); // 현재 스테이지 잔류 인원
+        int[] stageUsers = new int[N+2]; // int[] 는 내부값 자동으로 0으로 초기화
 
-            int reachPlayer = (int) Arrays.stream(stages)
-                    .filter(m -> m >= stage).count(); // 현재 스테이지 총 도달 인원
-
-            float failPercent;
-            if (herePlayer == 0) failPercent = 0;
-            else failPercent = (float)herePlayer / (float)reachPlayer;
-
-
-            stageMap.put(failPercent, i);
-            failPercentArr[i] = failPercent;
+        // stageUsers는 index == 스테이지, value == 스테이지 잔류 인원의 값을 지니는 배열이 된다.
+        for(int i = 1; i < stages.length; i++) {
+            stageUsers[stages[i]] += 1;
         }
 
-        Object[] sortedArr = Arrays.stream(failPercentArr)
-                        .sorted((o1,o2) -> Float.compare(o2, o1)).toArray();
-        // 문제 보기 시작한 7시 13분 ~ 이부분 까지 완료 7시 42분
-        System.out.println(stageMap.keySet());
-        System.out.println(Arrays.toString(sortedArr));
-
-        for(int i = 0; i < sortedArr.length; i++) {
-            int stage = stageMap.get((float) sortedArr[i]);
-            result[i] = stage;
+        double previousUsers = 0;
+        for(int j = 1; j <= N; j++) {
+            if(stageUsers[j] == 0) {
+                stageFailureMap.put(j, 0.0);
+                continue;
+            }
+            double failure = stageUsers[j] / ((double)stages.length - previousUsers);
+            previousUsers += stageUsers[j];
+            stageFailureMap.put(j, failure);
         }
 
-        return result;
+        return stageFailureMap.entrySet().stream().sorted((o1, o2) -> Double.compare(o2.getValue(), o1.getValue())).mapToInt(HashMap.Entry::getKey).toArray();
     }
 
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(solution(5, new int[] {2,1,2,6,2,4,3,3})));
     }
 }
